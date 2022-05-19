ce0f3815db6c16febdd539817bf0163a3409d608
= <=>
2022-05-19 11:41:59 -0500

    Agregando Prueba de unidad para la clase Reader

---
+++ test/utils/Reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require('./../../lib/utils/Reader')
+
+describe("Unit Test for Reader class",()=>{
+    test("Parte 1: metodo readJsonFile",()=>{
+        const explorers = Reader.readJsonFile("./visualpartners.json");
+        expect(explorers).not.toBeUndefined();
+    });
+});
