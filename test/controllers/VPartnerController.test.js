0f9b2403e7e3b65790484a93a2b92d07a56a422c
= <=>
2022-05-19 13:54:26 -0500

    Agregando prueba de unidad para el metodo obtener emails de alumnos con certificado VPartnerController

--- test/controllers/VPartnerController.test.js
+++ test/controllers/VPartnerController.test.js
@@ -4,5 +4,9 @@ describe('Unit test for VPartners Controller',()=>{
     test('Part 1: Prueba de unidad para getStudents',()=>{
         const students = VPartnerController.getAllVPartners();
         expect(students).not.toBeUndefined();
+    });
+    test('Part 2: Prueba de unidad para getEmailPartnersCert',()=>{
+        const emails = VPartnerController.getEmailPartnersCert('true');
+        expect(emails).toContain('Todd@visualpartnership.xyz')
     })
 })
