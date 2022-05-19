a79ed462f94b78729831afd24fa551fb01c16525
= <=>
2022-05-19 12:32:30 -0500

    Agregando prueba de unidad para metodo obtener el email de los alumnos con certificación de la clase VPartnerService

--- test/services/VPartnerServices.test.js
+++ test/services/VPartnerServices.test.js
@@ -1,11 +1,16 @@
 const Reader = require('./../../lib/utils/Reader');
+const vpartners = Reader.readJsonFile("./visualpartners.json");
+
 const VPartnerService= require('./../../lib/services/VPartnerServices')
 
 describe('Unit Test for visual partners Services',()=>{
     test('Part 1: Prueba de unidad impresion de todos los estudiantes',()=>{
-        const vpartners = Reader.readJsonFile("./visualpartners.json");
         const AllPartners = VPartnerService.getAllVPartners(vpartners);
 
         expect(AllPartners).not.toBeUndefined();
+    })
+    test('Part 2: Prueba de unidad email de alumnos con certificación',()=>{
+        const EmailPartners = VPartnerService.getEmailPartnersCert(vpartners);
+        expect(EmailPartners).toContain("Todd@visualpartnership.xyz")
     })
 })
